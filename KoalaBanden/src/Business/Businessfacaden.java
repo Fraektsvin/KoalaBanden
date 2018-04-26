63b1c1cc5906d82b3dac2c05088c08ac8f34fe8f
Jonas <Saltogpeber123@hotmail.com>
2018-04-26 09:55:01 +0200

    Oprettelse af sagsåbning

--- KoalaBanden/src/Business/Businessfacaden.java
+++ KoalaBanden/src/Business/Businessfacaden.java
@@ -5,6 +5,7 @@
  */
 package Business;
 
+import Acquaintance.IBusiness;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -17,35 +18,8 @@ import javafx.stage.Stage;
  *
  * @author Antonio
  */
-public class Businessfacaden extends Application {
+public class Businessfacaden implements IBusiness {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+ 
     
 }
---
+++ KoalaBanden/src/Business/CaseOpening.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Jonas
+ */
+public class CaseOpening {
+    private String  caseDescription; 
+    private boolean isSortHelpClear;
+    private boolean isInformedAboutRepresentationRights;
+    private boolean isInformedAboutElectronicRegistration;
+    private String  progressAgreement;
+    private boolean isConsentRelevant;
+    private String  citizenInvolvementSpecialCircumstances;
+    private String  actingMunicipality; 
+    private String  payingMunicipality;
+    
+    public CaseOpening() {
+        
+    }
+    
+    private void addGuardian() {
+        
+    }
+    
+    private void addRepresentative() {
+        
+    }
+    
+    private void createInquiry() {
+        
+    }
+    
+    private void addService() { 
+        
+    }
+    
+    private void addOffer() {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
