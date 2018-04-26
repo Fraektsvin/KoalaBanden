742261e6a4632dfa3e6a834a0a833b19ea52f566
ThomasFR89 <32256711+ThomasFR89@users.noreply.github.com>
2018-04-26 09:48:19 +0200

    Case and User classes added

---
+++ KoalaBanden/src/Business/Case.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author finch
+ */
+public class Case {
+    
+    // Data field
+    
+    private int ID;
+    private Date dateOfCreation;
+    private String lastModifiedBy;
+    private Date lastModified;
+    private int citizenSSN;
+
+    // Constructor
+    
+    public Case (int ID, int citizenSSN){
+        this.ID = ID;
+        this.citizenSSN = citizenSSN;
+    }
+    
+    // Methods
+    
+    public int getSSN(){
+        return citizenSSN;
+    }
+    
+}
---
+++ KoalaBanden/src/Business/User.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author finch
+ */
+public class User {
+    
+    // Date field
+    
+    private String username;
+    private String password;
+    private String email;
+    private int SSN;
+    private int accessLevel;
+    
+    // Constructor
+    
+    public User(String username, String password, String email, int SSN, int accessLevel){
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.SSN = SSN;
+        this.accessLevel = accessLevel;
+    }
+    
+    // Methods
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
