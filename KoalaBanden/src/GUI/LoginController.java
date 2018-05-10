57e5399f0e26ffbc30a2aee489e3ab3b9bfb1729
ThomasFR89 <32256711+ThomasFR89@users.noreply.github.com>
2018-05-10 11:48:56 +0200

    Errors corrected

---
+++ KoalaBanden/src/GUI/LoginController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author trium
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private JFXTextField loginuser;
+    @FXML
+    private JFXTextField loginpass;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void actionLogin(ActionEvent event) {
+    }
+    
+}
