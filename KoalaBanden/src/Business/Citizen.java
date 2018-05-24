159865a52a2b91911d39f81a2e642ac7efc1e568
ThomasFR89 <32256711+ThomasFR89@users.noreply.github.com>
2018-05-24 10:46:28 +0200

    citizen class corrected

--- KoalaBanden/src/Business/Citizen.java
+++ KoalaBanden/src/Business/Citizen.java
@@ -1,26 +1,66 @@
-   
 package Business;
 
-import Acquaintance.AccessLevel;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
  * @author finch
  */
-public class Citizen extends User {
-    
+public class Citizen {
+
+    // Data field
+    private int cpr;
+    private String address;
+    private String email;
+    private int phoneNumber;
+    private String firstname;
+    private String lastname;
+
     // Contructor
-    
-    public Citizen(String username, String password, String email, int SSN, AccessLevel accessLevel) {
-        super(username, password, email, SSN, accessLevel);
+    public Citizen(int cpr, String address, String email, int phoneNumber, String firstname, String lastname) {
+        this.cpr = cpr;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.firstname = firstname;
+        this.lastname = lastname;
     }
 
+    public Citizen(ResultSet rs) throws SQLException {
+        this.cpr = rs.getInt("cpr");
+        this.address = rs.getString("address");
+        this.email = rs.getString("email");
+        this.phoneNumber = rs.getInt("phoneNumber");
+        this.firstname = rs.getString("firstname");
+        this.lastname = rs.getString("lastname");
+    }
+
+    
     // Methods
     
-    public String getCaseInformation(){
-        String caseInformation = null;
-        return caseInformation;
+    public int getCpr() {
+        return cpr;
     }
 
-    
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
 }
